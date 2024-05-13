1718cc0fc67544192079497ffa85ac3b10f5285b
Joshua <siemajoshua4@gmail.com>
2024-05-13 17:32:42 +0300

    Made new changes in Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,66 +1,56 @@
-function Footer(){
-    return (
-    <div>
-    <div class="info">
-            <div>
-                <h1>50</h1>
-                <p>coffee varieties</p>
-            </div>
-        
-            <div>
-                <h1>70</h1>
-                <p>coffee brand</p>
-            </div>
-        
-            <div>
-                <h1>85</h1>
-                <p>side dish</p>
-            </div>
-        
-            <div>
-                <h1>2,000</h1>
-                <p>happy customers</p>
-            </div>
-            
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <h1>50</h1>
+            <h3>coffee varieties</h3>
+    
+        <div>
+            <h1>70</h1>
+            <h3>coffee brand</h3>
         </div>
     
-        <div class="last">
-            <div>
-                <h1>Dankuy</h1>
-                <h3>Lorem ispum dolor sit amet,consectetur adipicsing elit. Nullam in nibih vehicula.</h3>
-            </div>
-    
-            <div>
-                <h1>GetIn Touch</h1>
-                <p>140 Hope Street, Piano</p>
-                <p>anymail@mail.com</p>
-                <p>+1234567890</p>
-                <p>0900hrs - 1700hrs</p>
-            </div>
-    
-            <div>
-                <h1>Quicklinks</h1>
-                <p>Menu</p>
-                <p>Services</p>
-                <p>FAQs</p>
-                <p>Blog</p>
-                <p>Contact</p>
-            </div>
-    
-            <div>
-                <h1>Location</h1>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15955.143661862494!2d36.79271155!3d-1.3034532499999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10971278816f%3A0xedb9a8aec31f2f48!2sAshaki%20Grill%20-%20Kindaruma%20Rd!5e0!3m2!1sen!2ske!4v1708673766816!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
+        <div>
+            <h1>85</h1>
+            <h3>side dish</h3>
         </div>
-        
-        <div class="modal"> 
-            <div class="holder">
-                <button onclick="closeModal()">Close</button>
-                <p class="moda_info">
-                </p>
-            </div>    
+    
+        <div>
+            <h1>2,000</h1>
+            <h3>happy customers</h3>
         </div>
+        <div>
+            <h1>Dankuy</h1>
+            <h3>Lorem ispum dolor sit amet,consectetur adipicsing elit. Nullam in nibih vehicula.</h3>
+        </div>
+
+        <div>
+            <h1>GetIn Touch</h1>
+            <h3>140 Hope Street, Piano</h3>
+            <h3>anymail@mail.com</h3>
+            <h3>+1234567890</h3>
+            <h3>0900hrs - 1700hrs</h3>
+        </div>
+
+        <div>
+            <h1>Quicklinks</h1>
+            <h3>Menu</h3>
+            <h3>Services</h3>
+            <h3>FAQs</h3>
+            <h3>Blog</h3>
+            <h3>Contact</h3>
+        </div>
+
+        <div>
+            <h1>Location</h1>  
     </div>
-    )
-    }
-    export default Footer;
+    
+   
+            <button >Close</button>
+    
+    </>
+  )
+}
+
+export default Footer;
